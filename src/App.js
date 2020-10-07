29bc19d6c84b6f94a50aae94fd7f09bc26c5ee90
Robert Girardin <robertlgirardin@gmail.com>
2020-10-07 12:40:10 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import photo1 from "./images/chuttersnap-8I423fRMwjM-unsplash.jpg";
+import photo2 from "./images/chuttersnap-gDDas5_ALRw-unsplash.jpg";
+import photo3 from "./images/daria-lisovtsova-oNXK1F8cVaE-unsplash.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="overlay" />
+      <div className="heading">
+        <p>Life's work</p>
+      </div>
+      <div className="content">
+        <h1>some content</h1>
+      </div>
+      <div className="footing">
+        <p>Life's over</p>
+      </div>
+      <div
+        className="photo"
+        style={{ backgroundImage: `url(${photo1})`, backgroundSize: "cover" }}
+      ></div>
     </div>
   );
 }
