911d1ea21e8651419ee77df52caeaa880491cc0b
Robert Girardin <robertlgirardin@gmail.com>
2020-10-08 09:18:20 -0400

    api fetch and button finese

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import photo1 from "./images/chuttersnap-8I423fRMwjM-unsplash.jpg";
-import photo2 from "./images/chuttersnap-gDDas5_ALRw-unsplash.jpg";
-import photo3 from "./images/daria-lisovtsova-oNXK1F8cVaE-unsplash.jpg";
+import fwd from "./images/fwd.png";
+import back from "./images/back.png";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  let urls = [];
+  for (let i = 0; i < 4; i++) {
+    urls.push("https://source.unsplash.com/random/");
+  }
+  useEffect(() => {
+    const response = Promise.all(
+      urls.map((url, i) =>
+        fetch(url + i)
+          .then((data) => {
+            setPhotos((state) => [...state, data.url]);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      )
+    );
+  }, []);
   return (
     <div className="container">
-      <div className="overlay" />
+      <div className="overlay">
+        <div className="prev">
+          <img src={back} alt="" />
+        </div>
+        <div className="next">
+          <img src={fwd} alt="" />
+        </div>
+      </div>
       <div className="heading">
         <p>Life's work</p>
       </div>
       <div className="content">
-        <h1>some content</h1>
+        {photos.map((item) => (
+          <img src={item} key={item.id} alt={item.id} width="70%" />
+        ))}
       </div>
       <div className="footing">
         <p>Life's over</p>
---
+++ src/ipsum.js
@@ -0,0 +1,111 @@
+import React from "react";
+
+export default function Ipsum() {
+  return (
+    <div className="ipsum">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ornare massa eget
+        egestas purus viverra. Netus et malesuada fames ac turpis egestas
+        integer. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+        Porttitor massa id neque aliquam vestibulum morbi. Vitae proin sagittis
+        nisl rhoncus mattis rhoncus urna. Porta non pulvinar neque laoreet
+        suspendisse interdum consectetur libero id. Hac habitasse platea
+        dictumst vestibulum rhoncus est. Blandit turpis cursus in hac. Eros in
+        cursus turpis massa tincidunt dui. Accumsan tortor posuere ac ut
+        consequat semper. Donec ultrices tincidunt arcu non sodales neque
+        sodales ut. Arcu vitae elementum curabitur vitae. Et netus et malesuada
+        fames ac turpis. Ornare arcu dui vivamus arcu felis bibendum ut
+        tristique et. Nisl suscipit adipiscing bibendum est. At erat
+        pellentesque adipiscing commodo elit at imperdiet dui accumsan. Integer
+        eget aliquet nibh praesent tristique magna sit amet. Egestas egestas
+        fringilla phasellus faucibus scelerisque eleifend donec. Malesuada fames
+        ac turpis egestas. Turpis tincidunt id aliquet risus feugiat in ante
+        metus dictum. Et sollicitudin ac orci phasellus egestas tellus rutrum
+        tellus. Lorem dolor sed viverra ipsum. Urna neque viverra justo nec
+        ultrices. Amet mauris commodo quis imperdiet massa tincidunt nunc
+        pulvinar. Diam in arcu cursus euismod. Amet tellus cras adipiscing enim
+        eu turpis egestas pretium. Porta lorem mollis aliquam ut. Feugiat
+        scelerisque varius morbi enim nunc faucibus a pellentesque. Proin libero
+        nunc consequat interdum varius sit amet. Leo urna molestie at elementum.
+        Purus ut faucibus pulvinar elementum. Laoreet suspendisse interdum
+        consectetur libero id faucibus nisl tincidunt. Massa massa ultricies mi
+        quis hendrerit dolor magna. Viverra nibh cras pulvinar mattis nunc sed
+        blandit libero. Faucibus in ornare quam viverra orci. Dignissim
+        suspendisse in est ante in. Dui ut ornare lectus sit amet est placerat
+        in egestas. Habitant morbi tristique senectus et netus et malesuada
+        fames ac. Habitant morbi tristique senectus et. Nibh venenatis cras sed
+        felis eget velit aliquet sagittis id. Orci a scelerisque purus semper.
+        Augue lacus viverra vitae congue eu consequat ac. Facilisis sed odio
+        morbi quis commodo. Penatibus et magnis dis parturient montes nascetur
+        ridiculus mus. Integer feugiat scelerisque varius morbi. Dignissim enim
+        sit amet venenatis urna. Odio morbi quis commodo odio aenean sed.
+        Commodo quis imperdiet massa tincidunt nunc pulvinar sapien. Ullamcorper
+        velit sed ullamcorper morbi tincidunt. Fames ac turpis egestas sed
+        tempus urna et. Felis eget nunc lobortis mattis aliquam. Ipsum consequat
+        nisl vel pretium lectus quam id. Magna etiam tempor orci eu. Vulputate
+        sapien nec sagittis aliquam malesuada bibendum arcu. Non tellus orci ac
+        auctor augue mauris. Fringilla est ullamcorper eget nulla facilisi etiam
+        dignissim diam. Purus sit amet luctus venenatis lectus magna fringilla.
+        Aliquam id diam maecenas ultricies mi. Fermentum posuere urna nec
+        tincidunt praesent. Orci porta non pulvinar neque laoreet suspendisse
+        interdum. Vestibulum morbi blandit cursus risus at ultrices mi tempus
+        imperdiet. Morbi quis commodo odio aenean sed adipiscing. Felis eget
+        nunc lobortis mattis aliquam. Ipsum consequat nisl vel pretium lectus
+        quam. Nunc aliquet bibendum enim facilisis gravida neque convallis a
+        cras. Tortor at auctor urna nunc id cursus. Aliquam malesuada bibendum
+        arcu vitae elementum curabitur vitae. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ornare massa eget egestas purus viverra. Netus
+        et malesuada fames ac turpis egestas integer. Sed viverra ipsum nunc
+        aliquet bibendum enim facilisis gravida. Porttitor massa id neque
+        aliquam vestibulum morbi. Vitae proin sagittis nisl rhoncus mattis
+        rhoncus urna. Porta non pulvinar neque laoreet suspendisse interdum
+        consectetur libero id. Hac habitasse platea dictumst vestibulum rhoncus
+        est. Blandit turpis cursus in hac. Eros in cursus turpis massa tincidunt
+        dui. Accumsan tortor posuere ac ut consequat semper. Donec ultrices
+        tincidunt arcu non sodales neque sodales ut. Arcu vitae elementum
+        curabitur vitae. Et netus et malesuada fames ac turpis. Ornare arcu dui
+        vivamus arcu felis bibendum ut tristique et. Nisl suscipit adipiscing
+        bibendum est. At erat pellentesque adipiscing commodo elit at imperdiet
+        dui accumsan. Integer eget aliquet nibh praesent tristique magna sit
+        amet. Egestas egestas fringilla phasellus faucibus scelerisque eleifend
+        donec. Malesuada fames ac turpis egestas. Turpis tincidunt id aliquet
+        risus feugiat in ante metus dictum. Et sollicitudin ac orci phasellus
+        egestas tellus rutrum tellus. Lorem dolor sed viverra ipsum. Urna neque
+        viverra justo nec ultrices. Amet mauris commodo quis imperdiet massa
+        tincidunt nunc pulvinar. Diam in arcu cursus euismod. Amet tellus cras
+        adipiscing enim eu turpis egestas pretium. Porta lorem mollis aliquam
+        ut. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque.
+        Proin libero nunc consequat interdum varius sit amet. Leo urna molestie
+        at elementum. Purus ut faucibus pulvinar elementum. Laoreet suspendisse
+        interdum consectetur libero id faucibus nisl tincidunt. Massa massa
+        ultricies mi quis hendrerit dolor magna. Viverra nibh cras pulvinar
+        mattis nunc sed blandit libero. Faucibus in ornare quam viverra orci.
+        Dignissim suspendisse in est ante in. Dui ut ornare lectus sit amet est
+        placerat in egestas. Habitant morbi tristique senectus et netus et
+        malesuada fames ac. Habitant morbi tristique senectus et. Nibh venenatis
+        cras sed felis eget velit aliquet sagittis id. Orci a scelerisque purus
+        semper. Augue lacus viverra vitae congue eu consequat ac. Facilisis sed
+        odio morbi quis commodo. Penatibus et magnis dis parturient montes
+        nascetur ridiculus mus. Integer feugiat scelerisque varius morbi.
+        Dignissim enim sit amet venenatis urna. Odio morbi quis commodo odio
+        aenean sed. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien.
+        Ullamcorper velit sed ullamcorper morbi tincidunt. Fames ac turpis
+        egestas sed tempus urna et. Felis eget nunc lobortis mattis aliquam.
+        Ipsum consequat nisl vel pretium lectus quam id. Magna etiam tempor orci
+        eu. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Non
+        tellus orci ac auctor augue mauris. Fringilla est ullamcorper eget nulla
+        facilisi etiam dignissim diam. Purus sit amet luctus venenatis lectus
+        magna fringilla. Aliquam id diam maecenas ultricies mi. Fermentum
+        posuere urna nec tincidunt praesent. Orci porta non pulvinar neque
+        laoreet suspendisse interdum. Vestibulum morbi blandit cursus risus at
+        ultrices mi tempus imperdiet. Morbi quis commodo odio aenean sed
+        adipiscing. Felis eget nunc lobortis mattis aliquam. Ipsum consequat
+        nisl vel pretium lectus quam. Nunc aliquet bibendum enim facilisis
+        gravida neque convallis a cras. Tortor at auctor urna nunc id cursus.
+        Aliquam malesuada bibendum arcu vitae elementum curabitur vitae.
+      </p>
+    </div>
+  );
+}
